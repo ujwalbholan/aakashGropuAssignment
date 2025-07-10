bdb76793d7c52116e0b9f02e7ab4e77a39611926
ujwalbholan <ujwal.bholan022@apexcollege.edu.np>
2025-07-10 10:09:20 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className='bg-amber-950 text-amber-50'>hello prople</p>
+      </div>
+    </>
+  )
+}
+
+export default App
